2abe05b2aa47ed00fec4dc7e7b66960df3dbc25f
rohit001007 <rohit001007@gmail.com>
2020-12-05 20:51:58 +0530

    Added : Problem Solve Using Switch Case

--- Employee.java
+++ Employee.java
@@ -4,23 +4,25 @@ public class Employee {
 
     public static void main(String[] args) {
 
-        int FullTime=1;
-        int PartTime=2;
-        int WagePerHr=20;
-        int WorkPerDay=8;
-        int WorkPartTime=4;
+        int WagePerHr = 20;
+        int WorkPerDay = 8;
+        int WorkPartTime = 4;
 
-        double empCheck=Math.floor((Math.random()*10)%3);
+        int empCheck = (int) Math.floor((Math.random() * 10) % 3);
 
-        if (FullTime==empCheck) {
-            int WagePerDay = (WagePerHr * WorkPerDay);
-            System.out.println("\n * The Full Day Wage Of Employee Is : " + WagePerDay);
+        switch (empCheck) {
+            case 1:
+                System.out.println("\n * Employee Is Full Time");
+                int WagePerDay = (WagePerHr * WorkPerDay);
+                System.out.println("\n * The Daily Wage Of Employee Is :" + WagePerDay);
+                break;
+            case 2:
+                System.out.println("\n * Employee Is Part Time");
+                int WagePartTime = (WagePerHr * WorkPartTime);
+                System.out.println("\n * The Part Time Wage Of Employee Is :" + WagePartTime);
+                break;
+            default:
+                System.out.println("\n * Employee Is Absent....!!");
         }
-        else if (PartTime==empCheck) {
-            int WagePartTime = (WagePerHr * WorkPartTime);
-            System.out.println("\n * The Part Time Wage Of Employee Is : " + WagePartTime);
-        }
-        else
-            System.out.println("\n * Employee Is Absent....!!");
     }
 }
