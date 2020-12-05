46a96ebb35ee58c7518e26cfd56461776a8f89b6
rohit001007 <rohit001007@gmail.com>
2020-12-05 20:37:23 +0530

    Added : Check Employee Attendance

--- Employee.java
+++ Employee.java
@@ -2,8 +2,14 @@ package com.Bridgelabz;
 
 public class Employee {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
+        int FullTime=1;
+        double empCheck=Math.floor((Math.random()*10)%2);
+        if (FullTime==empCheck){
+            System.out.println("\n * Employee Is Present...!");
+        }
+        else
+            System.out.println("\n * Employee Is Absent...!");
 
-        System.out.printf("\n * Welcome To Employee Wages Problem In Java * \n");
     }
 }
