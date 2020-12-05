a02d9e321b099dd448d9432f34671591b0d6ed1f
rohit001007 <rohit001007@gmail.com>
2020-12-05 20:32:33 +0530

    Added : Welcome To Employee Wages Problem In Java

---
+++ Employee.java
@@ -0,0 +1,9 @@
+package com.Bridgelabz;
+
+public class Employee {
+
+    public static void main(String args[]) {
+
+        System.out.printf("\n * Welcome To Employee Wages Problem In Java * \n");
+    }
+}
