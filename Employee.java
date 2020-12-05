765780eee06eb58cefbad3727a373db5f5e9e0f1
rohit001007 <rohit001007@gmail.com>
2020-12-05 21:21:14 +0530

    Added : Employee Wages For Seprate Company

--- Employee.java
+++ Employee.java
@@ -1,40 +1,60 @@
 package com.Bridgelabz;
 
 public class Employee {
-    public static final int IsPartTime = 1;
-    public static final int IsFullTime = 2;
 
-    public static void CalculateWage(String a,int WagePerHr,int WorkingDays,int MaxHrsInMonth){
-        int EmpHr = 0;
-        int TotalEmpHr = 0;
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+    private final String Company;
+    private final int empRatePerHour;
+    private final int numOfWorkingDays;
+    private final int maxHoursPerMonth;
+
+    Employee(String Company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+
+        this.Company = Company;
+        this.empRatePerHour = empRatePerHour;
+        this.numOfWorkingDays = numOfWorkingDays;
+        this.maxHoursPerMonth = maxHoursPerMonth;
+
+    }
+
+    private int Calculate() {
+
+        int empHrs = 0;
+        int empWage = 0;
         int TotalEmpWage = 0;
-        int TotalWorkingDay =0;
+        int TotalWorkingHours = 0;
+        int day = 0;
 
-        while (TotalEmpHr < MaxHrsInMonth && TotalWorkingDay <= WorkingDays) {
-            TotalWorkingDay++;
-            int empCheck = (int) Math.floor(Math.random() * 10 % 3);
-
-            switch (empCheck) {
-                case IsPartTime:
-                    EmpHr = 4;
+        while (day < numOfWorkingDays || TotalWorkingHours <= maxHoursPerMonth) {
+            double empCheck = Math.floor(Math.random() * 10 % 3);
+            day++;
+            switch ((int) empCheck) {
+                case IS_FULL_TIME:
+                    empHrs = 8;
                     break;
-                case IsFullTime:
-                    EmpHr = 8;
+                case IS_PART_TIME:
+                    empHrs = 4;
                     break;
                 default:
-                    EmpHr = 0;
-                    break;
+                    empHrs = 0;
+
             }
-            TotalEmpHr += EmpHr;
-            System.out.println("\n Day#: "+TotalWorkingDay+" Employee Woks :"+EmpHr+"Hrs");
+            TotalWorkingHours += empHrs;
+            empWage = empHrs * empRatePerHour;
+            TotalEmpWage += empWage;
         }
-        TotalEmpWage = TotalEmpHr*WagePerHr;
-        System.out.println("\n --> For Company "+ a + " The Total Wage Of Employee Is :" + TotalEmpWage);
+        return TotalEmpWage;
+
     }
-    public static void main(String[] args){
-        CalculateWage("Apple",30,5,10);
-        CalculateWage("Google",50,3,10);
-        CalculateWage("Microsoft",25,5,10);
-        CalculateWage("Orale",15,10,50);
+
+    public static void main(String[] args) {
+        Employee Apple = new Employee("Apple", 20, 20, 100);
+        Employee Google = new Employee("Google", 30, 20, 80);
+        Employee Microsoft = new Employee("Microsoft", 30, 20, 80);
+
+        System.out.println("\nTotal Employee Wage Of Apple Company Is : " + Apple.Calculate());
+        System.out.println("Total Employee Wage Of Google Company Is : " + Google.Calculate());
+        System.out.println("Total Employee Wage Of Microsoft Company Is : " + Microsoft.Calculate());
     }
 }
