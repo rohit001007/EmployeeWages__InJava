2526b354129dd599d33bf0f7cf2ce0c2d497da04
rohit001007 <rohit001007@gmail.com>
2020-12-05 20:47:24 +0530

    Added : The Part Time Or Full Time Wage Of Employee

--- Employee.java
+++ Employee.java
@@ -3,15 +3,22 @@ package com.Bridgelabz;
 public class Employee {
 
     public static void main(String[] args) {
+
         int FullTime=1;
+        int PartTime=2;
         int WagePerHr=20;
         int WorkPerDay=8;
+        int WorkPartTime=4;
 
-        double empCheck=Math.floor((Math.random()*10)%2);
-        if (FullTime==empCheck){
-            System.out.println("\n * Employee Is Present.");
-            int WagePerDay=( WagePerHr*WorkPerDay);
-            System.out.println(" * The Daily Wage Of Employee Is : "+WagePerDay);
+        double empCheck=Math.floor((Math.random()*10)%3);
+
+        if (FullTime==empCheck) {
+            int WagePerDay = (WagePerHr * WorkPerDay);
+            System.out.println("\n * The Full Day Wage Of Employee Is : " + WagePerDay);
+        }
+        else if (PartTime==empCheck) {
+            int WagePartTime = (WagePerHr * WorkPartTime);
+            System.out.println("\n * The Part Time Wage Of Employee Is : " + WagePartTime);
         }
         else
             System.out.println("\n * Employee Is Absent....!!");
