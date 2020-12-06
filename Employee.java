b1c67087b676906b661a01144acb802347976c8b
rohit001007 <rohit001007@gmail.com>
2020-12-06 20:45:48 +0530

    Added : Employee's Daily And Monthly Wage

--- Employee.java
+++ Employee.java
@@ -1,68 +1,42 @@
 package com.Bridgelabz;
 
-import java.util.HashMap;
-import java.util.ArrayList;
-import java.util.Map;
+public class Employee{
 
-public class Employee implements IComputeEmpWage {
+    void empSalary() {
 
-    public static final int IS_FULL_TIME = 1;
-    public static final int IS_PART_TIME = 2;
+        int isFullTime = 1;
+        int isPartTime = 0;
+        int empRatePrHr = 20;
+        int empHr = 0;
+        int perDaySalary = 0;
+        int mothWorkingDay = 20;
+        int monthlySalary = 0;
+        int monthyHrs = 0;
 
-    private ArrayList<ComapnyEmpWage> comapnyEmpWageList;
-    private Map<String, ComapnyEmpWage> companyToEmpWageMap;
+        for (int i = 1;i <= mothWorkingDay && monthyHrs <= 100;i++) {
 
-    private Employee() {
-        comapnyEmpWageList = new ArrayList<>();
-        companyToEmpWageMap = new HashMap<>();
-    }
+            int empCheck = (int) Math.floor(Math.random() * 10) % 2;
 
-    public void addComapnyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursInMonth) {
-        ComapnyEmpWage comapnyEmpWage = new ComapnyEmpWage(company, empRatePerHour, numOfWorkingDays, maxHoursInMonth);
-        comapnyEmpWageList.add(comapnyEmpWage);
-        companyToEmpWageMap.put(company, comapnyEmpWage);
-    }
+                switch ((int)empCheck) {
 
-    public void computeEmpWage() {
-        for (int i = 0; i < comapnyEmpWageList.size(); i++) {
-            ComapnyEmpWage comapnyEmpWage = comapnyEmpWageList.get(i);
-            comapnyEmpWage.setTotalEmpWage(this.computeEmpWage(comapnyEmpWage));
-            System.out.println(comapnyEmpWage);
-        }
-    }
-
-    @Override
-    public int getTotalWage(String comapny) {
-        return companyToEmpWageMap.get(comapny).totalEmpWage;
-    }
-
-    public int computeEmpWage(ComapnyEmpWage comapnyEmpWage) {
-        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-
-        while (totalEmpHrs <= comapnyEmpWage.maxHoursInMonth && totalWorkingDays < comapnyEmpWage.numOfWorkingDays) {
-            totalWorkingDays++;
-            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-            switch (empCheck) {
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
+                    case 1 :
+                        empHr = 8;
+                        break;
+                    case 0 :
+                        empHr = 4;
+                        break;
+                }
+                monthyHrs = monthyHrs + empHr;
+                System.out.println("\n Day#: " + i + " Employee Working Hrs : " + monthyHrs);
+                perDaySalary = empRatePrHr * empHr;
+                System.out.println(perDaySalary);
+                monthlySalary = perDaySalary + monthlySalary;
             }
-            totalEmpHrs += empHrs;
-            System.out.println("\n Day#: " + totalWorkingDays + " Employee Woks : " + empHrs);
+            System.out.println("\n * The Monthly Employee Wage Is : "+monthlySalary);
         }
-        return totalEmpHrs * comapnyEmpWage.empRatePerHour;
-    }
+        public static void main(String[] args) {
 
-    public static void main(String[] args) {
-        IComputeEmpWage empWage = new Employee();
-        empWage.addComapnyEmpWage("Apple", 20, 20, 100);
-        empWage.addComapnyEmpWage("Google", 30, 20, 80);
-        empWage.addComapnyEmpWage("Microsoft", 30, 20, 80);
-        empWage.computeEmpWage();
+            Employee empWage = new Employee();
+            empWage.empSalary();
     }
 }
--- IComputeEmpWage.java
+++
@@ -1,8 +0,0 @@
-package com.Bridgelabz;
-
-public interface IComputeEmpWage {
-
-    public void addComapnyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursInMonth);
-    public void computeEmpWage();
-    public int getTotalWage(String company);
-}
